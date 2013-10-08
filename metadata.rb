498e30cbc874b5b8750f30c993484be453ebc879
Vince Montalbano <vince.montalbano@gmail.com>
2013-10-08 09:16:22 -0500

    Initial cookbook

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Acquity Group"
+maintainer_email "vince.montalbano@acquitygroup.com"
+license          "Apache 2.0"
+description      "Installs/Configures Oracle 11g Express Edition"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
