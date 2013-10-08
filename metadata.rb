5985977d8cbed67274436eb00851d083cbfd33b3
Vince Montalbano <vince.montalbano@gmail.com>
2013-10-08 11:43:38 -0500

    work in progress

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Acquity Group"
 maintainer_email "vince.montalbano@acquitygroup.com"
 license          "Apache 2.0"
-description      "Installs/Configures Oracle 11g Express Edition"
+description      "Installs/Configures Oracle 11g Express Edition via Chef on CentOS"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
--- recipes/default.rb
+++ recipes/default.rb
@@ -16,3 +16,54 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# https://gist.github.com/ricardovf/1005206
+
+default_temp_dir = "oracle_tmp"
+temp_dir = node["oracle-11g-ee"][:temp_dir] || default_temp_dir
+
+extract_script = (Pathname.new(temp_dir) + 'extract-source.sh').to_s
+install_script = (Pathname.new(temp_dir) + 'install-mono.sh').to_s
+
+default_pack_list = ["libaio", "bc", "flex"]
+pack_list = node["oracle-11g-ee"][:pack_list] || default_pack_list
+
+# Install prereq packages
+pack_list.each do |pkg|
+  package pkg
+end
+
+# Create temp_dir for oracle ops
+directory temp_dir do
+  recursive true
+  owner 'root'
+  group 'root'
+  mode '0644'
+  action :create
+end
+
+# Unzip the oracle zipfile
+
+
+
+
+# unzip -q oracle-xe-11.2.0-1.0.x86_64.rpm.zip
+# cd Disk1
+# prepare xe.rsp
+
+# ORACLE_LISTENER_PORT: A valid listener numeric port value, so that you can connect to Oracle Database XE
+# ORACLE_HTTP_PORT: A valid HTTP port numeric value for Oracle Application Express
+# ORACLE_PASSWORD: A password value for the SYS and SYSTEM administrative user accounts
+# ORACLE_CONFIRM_PASSWORD: The SYS and SYSTEM password value again, to confirm it
+# ORACLE_DBENABLE: Yes (y) or no (n), to specify whether you want to start Oracle Database XE automatically when the computer starts
+
+#!/bin/bash
+# rpm -ivh  /downloads/oracle-xe-11.2.0-1.0.x86_64 > /xe_logs/XEsilentinstall.log
+# /etc/init.d/oracle-xe configure responseFIle=<location of xe.rsp> >> /xe_logs/XEsilentinstall.log
+
+# run
+# /u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh
+
+# You may also want edit your login or profile files so that these environment variables are set properly each time you log in or open a new shell.
+# For Bourne, Bash, or Korn shell, enter the following line into the .bash_profile (to log in) or .bashrc file (to open a new shell):
+# . /u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh
