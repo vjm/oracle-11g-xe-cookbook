d83c75a9f8a6ba9b51b56014fa39f6fef6767610
Vince Montalbano <vince.montalbano@gmail.com>
2013-10-08 14:39:31 -0500

    properly using node.default[]

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,11 @@
-node["oracle-11g-ee"][:temp_dir] = "/oracle_tmp"
-node["oracle-11g-ee"][:pack_list] = ["libaio", "bc", "flex"]
+node.default["oracle-11g-ee"][:temp_dir] = "/oracle_tmp"
+node.default["oracle-11g-ee"][:pack_list] = ["libaio", "bc", "flex"]
 
-node["oracle-11g-ee"][:oracle_zipfile] = ["/vagrant/oracle-xe-11.2.0-1.0.x86_64.rpm.zip"]
+node.default["oracle-11g-ee"][:oracle_zipfile] = ["/vagrant/oracle-xe-11.2.0-1.0.x86_64.rpm.zip"]
 
-node["oracle-11g-ee"][:oracle_http_port] = 8080
-node["oracle-11g-ee"][:oracle_listener_port] = 1521
-node["oracle-11g-ee"][:oracle_password] = "vagrant"
-node["oracle-11g-ee"][:oracle_dbenable] = "y"
+node.default["oracle-11g-ee"][:oracle_http_port] = 8080
+node.default["oracle-11g-ee"][:oracle_listener_port] = 1521
+node.default["oracle-11g-ee"][:oracle_password] = "vagrant"
+node.default["oracle-11g-ee"][:oracle_dbenable] = "y"
 
-node["oracle-11g-ee"][:oracle_env_path] = "/u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh"
+node.default["oracle-11g-ee"][:oracle_env_path] = "/u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh"
