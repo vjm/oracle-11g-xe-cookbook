b2bc53c35502e7488a922fd16597bbd620cee645
Vince Montalbano <vince.montalbano@gmail.com>
2013-10-08 14:32:19 -0500

    first full attempt

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,53 +17,103 @@
 # limitations under the License.
 #
 
+# http://docs.oracle.com/cd/E17781_01/install.112/e18802/toc.htm#BABCCGCF
 # https://gist.github.com/ricardovf/1005206
 
-default_temp_dir = "oracle_tmp"
-temp_dir = node["oracle-11g-ee"][:temp_dir] || default_temp_dir
+extract_script = (Pathname.new(node["oracle-11g-ee"][:temp_dir]) + 'extract-zipfiles.sh').to_s
+oracle_rpm_script = (Pathname.new(node["oracle-11g-ee"][:temp_dir]) + 'oracle_rpm_install.sh').to_s
+xe_rsp = (Pathname.new(node["oracle-11g-ee"][:temp_dir]) + 'xe.rsp').to_s
+oracle_rpm_path = (Pathname.new(node["oracle-11g-ee"][:temp_dir]) + 'Disk1/oracle-xe-11.2.0-1.0.x86_64.rpm').to_s
 
-extract_script = (Pathname.new(temp_dir) + 'extract-source.sh').to_s
-install_script = (Pathname.new(temp_dir) + 'install-mono.sh').to_s
-
-default_pack_list = ["libaio", "bc", "flex"]
-pack_list = node["oracle-11g-ee"][:pack_list] || default_pack_list
+#make sure it's an array. http://www.ruby-doc.org/core-1.9.3/Kernel.html#method-i-Array
+zips_to_extract = Array([node["oracle-11g-ee"][:oracle_zipfile]) 
 
 # Install prereq packages
-pack_list.each do |pkg|
-  package pkg
+node["oracle-11g-ee"][:pack_list].each do |pkg|
+	package pkg
 end
 
 # Create temp_dir for oracle ops
-directory temp_dir do
-  recursive true
-  owner 'root'
-  group 'root'
-  mode '0644'
-  action :create
+directory node["oracle-11g-ee"][:temp_dir] do
+	recursive true
+	owner 'root'
+	group 'root'
+	mode '0644'
+	action :create
 end
 
 # Unzip the oracle zipfile
+template extract_script do
+	source 'extract-zipfiles.sh.erb'
+	owner 'root'
+	group 'root'
+	mode '0755'
+	variables({
+		:extract_to => node["oracle-11g-ee"][:temp_dir],
+		:zips_to_extract => zips_to_extract
+		})
+	action :create_if_missing
+	notifies :run, "bash[extract-zipfiles]"
+end
 
+template 
 
+# Run the extract script - only runs if notified with :run by extract_script template indicating successful unzip
+bash 'extract-zipfiles' do
+	code extract_script
+	user 'root'
+	action :nothing
+end
 
+# #{xe_rsp} is where this file will be copied to from the 'source'.
+template xe_rsp do 
+	source 'xe.rsp.erb'
+	owner 'root'
+	group 'root'
+	mode '0444'
+	variables({
+		:oracle_http_port => node["oracle-11g-ee"][:oracle_http_port]
+		:oracle_listener_port => node["oracle-11g-ee"][:oracle_listener_port]
+		:oracle_password => node["oracle-11g-ee"][:oracle_password]
+		:oracle_confirm_password => node["oracle-11g-ee"][:oracle_password]
+		:oracle_dbenable => node["oracle-11g-ee"][:oracle_dbenable]
+		})
+	action :create_if_missing
+end
 
-# unzip -q oracle-xe-11.2.0-1.0.x86_64.rpm.zip
-# cd Disk1
-# prepare xe.rsp
+template oracle_rpm_script do 
+	source 'oracle_rpm_install.sh.erb'
+	owner 'root'
+	group 'root'
+	mode '0755'
+	variables({
+		:oracle_rpm_path => oracle_rpm_path
+		:xe_path => xe_rsp
+		})
+	action :create_if_missing
+	notifies :run, "bash[oracle-rpm-install]"
+end
 
-# ORACLE_LISTENER_PORT: A valid listener numeric port value, so that you can connect to Oracle Database XE
-# ORACLE_HTTP_PORT: A valid HTTP port numeric value for Oracle Application Express
-# ORACLE_PASSWORD: A password value for the SYS and SYSTEM administrative user accounts
-# ORACLE_CONFIRM_PASSWORD: The SYS and SYSTEM password value again, to confirm it
-# ORACLE_DBENABLE: Yes (y) or no (n), to specify whether you want to start Oracle Database XE automatically when the computer starts
+# Run the install script - only runs if notified with :run by oracle_rpm_script template
+bash 'oracle-rpm-install' do
+	code oracle_rpm_script
+	user 'root'
+	action :nothing
+	notifies :run, "bash[oracle_env]"
+end
 
-#!/bin/bash
-# rpm -ivh  /downloads/oracle-xe-11.2.0-1.0.x86_64 > /xe_logs/XEsilentinstall.log
-# /etc/init.d/oracle-xe configure responseFIle=<location of xe.rsp> >> /xe_logs/XEsilentinstall.log
+bash 'oracle_env' do
+	code node["oracle-11g-ee"][:oracle_env_path]
+	user 'root'
+	action :nothing
+	notifies :run, "bash[append-to-bashrc]"
+end
 
-# run
-# /u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh
-
-# You may also want edit your login or profile files so that these environment variables are set properly each time you log in or open a new shell.
-# For Bourne, Bash, or Korn shell, enter the following line into the .bash_profile (to log in) or .bashrc file (to open a new shell):
-# . /u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh
+bash "append-to-bashrc" do
+    user "root"
+    cwd node["oracle-11g-ee"][:temp_dir]
+    code <<-EOT
+       echo ". #{node['oracle-11g-ee'][:oracle_env_path]}" >> /root/.bashrc
+    EOT
+    action :nothing
+end
