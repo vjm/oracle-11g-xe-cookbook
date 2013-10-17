49ce35f024fa0a8f9f116cfb155ed72ab0f7083a
Vince Montalbano <vince.montalbano@gmail.com>
2013-10-17 13:23:10 -0500

    refactor and using rpm directly instead of zip

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,11 @@
 node.default["oracle-11g-ee"][:temp_dir] = "/oracle_tmp"
 node.default["oracle-11g-ee"][:pack_list] = ["libaio", "bc", "flex", "zip", "unzip"]
 
-node.default["oracle-11g-ee"][:oracle_zipfile] = ["/vagrant/oracle-xe-11.2.0-1.0.x86_64.rpm.zip"]
+node.default["oracle-11g-ee"][:rpm_url] = nil
 
 node.default["oracle-11g-ee"][:oracle_http_port] = 8080
 node.default["oracle-11g-ee"][:oracle_listener_port] = 1521
 node.default["oracle-11g-ee"][:oracle_password] = "vagrant"
-node.default["oracle-11g-ee"][:oracle_dbenable] = "y"
+node.default["oracle-11g-ee"][:oracle_dbenable] = true
 
 node.default["oracle-11g-ee"][:oracle_env_path] = "/u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh"
--- recipes/default.rb
+++ recipes/default.rb
@@ -20,17 +20,29 @@
 # http://docs.oracle.com/cd/E17781_01/install.112/e18802/toc.htm#BABCCGCF
 # https://gist.github.com/ricardovf/1005206
 
-extract_script = (Pathname.new(node["oracle-11g-ee"][:temp_dir]) + 'extract-zipfiles.sh').to_s
-oracle_rpm_script = (Pathname.new(node["oracle-11g-ee"][:temp_dir]) + 'oracle_rpm_install.sh').to_s
+# extract_script = (Pathname.new(node["oracle-11g-ee"][:temp_dir]) + 'extract-zipfiles.sh').to_s
+
+oracle_rpm_name = 'oracle-xe-11.2.0-1.0.x86_64.rpm'
+# oracle_rpm_script = (Pathname.new(node["oracle-11g-ee"][:temp_dir]) + 'oracle_rpm_install.sh').to_s
+oracle_logs_dir = ((Pathname.new(node["oracle-11g-ee"][:temp_dir])) + '/xe_logs').to_s
+oracle_log_file = oracle_logs_dir + 'XEsilentinstall.log'
 xe_rsp = (Pathname.new(node["oracle-11g-ee"][:temp_dir]) + 'xe.rsp').to_s
-oracle_rpm_path = (Pathname.new(node["oracle-11g-ee"][:temp_dir]) + 'Disk1/oracle-xe-11.2.0-1.0.x86_64.rpm').to_s
+oracle_rpm_path = (File.join(Chef::Config[:file_cache_path], oracle_rpm_name)).to_s
 
 #make sure it's an array. http://www.ruby-doc.org/core-1.9.3/Kernel.html#method-i-Array
-zips_to_extract = Array(node["oracle-11g-ee"][:oracle_zipfile]) 
+# zips_to_extract = Array(node["oracle-11g-ee"][:oracle_zipfile]) 
 
 # Install prereq packages
 node["oracle-11g-ee"][:pack_list].each do |pkg|
 	package pkg
+end
+
+remote_file oracle_rpm_path do
+	source node['oracle-11g-ee'][:rpm_url]
+	owner 'root'
+	group 'root'
+	mode '0644'
+	action :create
 end
 
 # Create temp_dir for oracle ops
@@ -42,76 +54,43 @@ directory node["oracle-11g-ee"][:temp_dir] do
 	action :create
 end
 
-# Unzip the oracle zipfile
-template extract_script do
-	source 'extract-zipfiles.sh.erb'
-	owner 'root'
-	group 'root'
-	mode '0755'
-	variables({
-		:extract_to => node["oracle-11g-ee"][:temp_dir],
-		:zips_to_extract => zips_to_extract
-		})
-	action :create_if_missing
-	notifies :run, "bash[extract-zipfiles]"
-end
-
-# Run the extract script - only runs if notified with :run by extract_script template indicating successful unzip
-bash 'extract-zipfiles' do
-	code extract_script
-	user 'root'
-	action :nothing
-end
-
 # #{xe_rsp} is where this file will be copied to from the 'source'.
 template xe_rsp do 
 	source 'xe.rsp.erb'
 	owner 'root'
 	group 'root'
 	mode '0444'
-	variables({
-		:oracle_http_port => node["oracle-11g-ee"][:oracle_http_port],
-		:oracle_listener_port => node["oracle-11g-ee"][:oracle_listener_port],
-		:oracle_password => node["oracle-11g-ee"][:oracle_password],
-		:oracle_confirm_password => node["oracle-11g-ee"][:oracle_password],
-		:oracle_dbenable => node["oracle-11g-ee"][:oracle_dbenable]
-		})
-	action :create_if_missing
+	action :create
 end
 
-template oracle_rpm_script do 
-	source 'oracle_rpm_install.sh.erb'
+# Create temp_dir for oracle logs
+directory oracle_logs_dir do
+	recursive true
 	owner 'root'
 	group 'root'
-	mode '0755'
-	variables({
-		:oracle_rpm_path => oracle_rpm_path,
-		:xe_path => xe_rsp
-		})
-	action :create_if_missing
-	notifies :run, "bash[oracle-rpm-install]"
+	mode '0644'
+	action :create
 end
 
-# Run the install script - only runs if notified with :run by oracle_rpm_script template
-bash 'oracle-rpm-install' do
-	code oracle_rpm_script
-	user 'root'
-	action :nothing
-	notifies :run, "bash[oracle_env]"
+# we use this instead of package because we want to log our output to this file.
+execute 'oracle-xe-rpm'
+	command "rpm -ivh #{oracle_rpm_path} > #{oracle_log_file}"
+	notifies :run, "execute['configure-oracle']"
 end
 
-bash 'oracle_env' do
-	code node["oracle-11g-ee"][:oracle_env_path]
-	user 'root'
-	action :nothing
-	notifies :run, "bash[append-to-bashrc]"
+# package 'oracle-xe' do
+# 	source oracle_rpm_path
+# 	action :install
+# end
+
+execute 'configure-oracle' do
+	command "/etc/init.d/oracle-xe configure responseFile=#{xe_rsp} >> #{oracle_log_file}"
+	creates "/u01/app/oracle/oradata"
+	action :nothing # only runs if execute['oracle-xe-rpm'] runs properly
+	returns [0,1] # don't care if it's already configured
 end
 
-bash "append-to-bashrc" do
-    user "root"
-    cwd node["oracle-11g-ee"][:temp_dir]
-    code <<-EOT
-       echo ". #{node['oracle-11g-ee'][:oracle_env_path]}" >> /etc/profile.d/oracle.sh
-    EOT
-    action :nothing
+link "/etc/profile.d/oracle_env.sh" do
+  to node["oracle-11g-ee"][:oracle_env_path]
+  action :create
 end
