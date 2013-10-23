0eb772bd164e1ce70663572a3d8e08f40c137d48
Vince Montalbano <vince.montalbano@gmail.com>
2013-10-22 22:28:59 -0500

    bump to 0.3
    Signed-off-by: Vince Montalbano <vince.montalbano@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "vince.montalbano@acquitygroup.com"
 license          "Apache 2.0"
 description      "Installs/Configures Oracle 11g Express Edition via Chef on CentOS"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.2.1"
+version          "0.3"
 
 
 # depends "iptables"
