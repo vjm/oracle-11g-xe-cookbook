4273c6f2050f5d66bc5b3ae24e2c63fe9a523d77
Vince Montalbano <vince.montalbano@gmail.com>
2013-10-29 16:21:58 -0500

    Bump to version 0.5
    Signed-off-by: Vince Montalbano <vince.montalbano@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "vince.montalbano@acquitygroup.com"
 license          "Apache 2.0"
 description      "Installs/Configures Oracle 11g Express Edition via Chef on CentOS"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.4"
+version          "0.5"
 
 recipe "chef-oracle-11g-xe", "Includes the service recipe by default."
 
